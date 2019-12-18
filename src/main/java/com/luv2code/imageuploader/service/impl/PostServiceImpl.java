cd9ac8998babf01cc81956eee4173b9fa6df8129
Luka Žugaj <lzugaj@tvz.hr>
2019-12-18 15:06:19 +0100

    Merge two branches and change some small issues

--- src/main/java/com/luv2code/imageuploader/service/impl/PostServiceImpl.java
+++ src/main/java/com/luv2code/imageuploader/service/impl/PostServiceImpl.java
@@ -1,21 +1,5 @@
 package com.luv2code.imageuploader.service.impl;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
-
 import com.luv2code.imageuploader.entity.Post;
 import com.luv2code.imageuploader.entity.User;
 import com.luv2code.imageuploader.repository.PostRepository;
@@ -30,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.*;
 
 /**
@@ -189,7 +174,7 @@ public class PostServiceImpl implements PostService {
 		for (Post post : posts) {
 			if (post.getHashTag().contains(hashTag) &&
 					Double.valueOf(post.getImageFileSize()) >= sizeFrom &&
-					post.getDateOfPost().isAfter(LocalDateTime.parse((CharSequence) dateFrom)) &&
+					post.getDateOfPost().isAfter(dateFrom.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) &&
 					post.getUser().getUserName().contains(author)) {
 				foundedPosts.add(post);
 			}
@@ -206,7 +191,7 @@ public class PostServiceImpl implements PostService {
 			if (post.getHashTag().contains(hashTag) &&
 					Double.valueOf(post.getImageFileSize()) >= sizeFrom &&
 					Double.valueOf(post.getImageFileSize()) <= sizeTo &&
-					post.getDateOfPost().isAfter(LocalDateTime.parse((CharSequence) dateFrom)) &&
+					post.getDateOfPost().isAfter(dateFrom.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) &&
 					post.getUser().getUserName().contains(author)) {
 				foundedPosts.add(post);
 			}
@@ -222,8 +207,8 @@ public class PostServiceImpl implements PostService {
 		for (Post post : posts) {
 			if (post.getHashTag().contains(hashTag) &&
 					Double.valueOf(post.getImageFileSize()) >= sizeFrom &&
-					post.getDateOfPost().isAfter(LocalDateTime.parse((CharSequence) dateFrom)) &&
-					post.getDateOfPost().isBefore(LocalDateTime.parse((CharSequence) dateTo)) &&
+					post.getDateOfPost().isAfter(dateFrom.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) &&
+					post.getDateOfPost().isBefore(dateTo.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) &&
 					post.getUser().getUserName().contains(author)) {
 				foundedPosts.add(post);
 			}
@@ -240,14 +225,15 @@ public class PostServiceImpl implements PostService {
 			if (post.getHashTag().contains(hashTag) &&
 					Double.valueOf(post.getImageFileSize()) >= sizeFrom &&
 					Double.valueOf(post.getImageFileSize()) <= sizeTo &&
-					post.getDateOfPost().isAfter(LocalDateTime.parse((CharSequence) dateFrom)) &&
-					post.getDateOfPost().isBefore(LocalDateTime.parse((CharSequence) dateTo)) &&
+					post.getDateOfPost().isAfter(dateFrom.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) &&
+					post.getDateOfPost().isBefore(dateTo.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()) &&
 					post.getUser().getUserName().contains(author)) {
 				foundedPosts.add(post);
 			}
 		}
 
 		return foundedPosts;
+	}
 
 	public Map<Long, Post> mapAllPosts(List<Post> posts) {
 		Map<Long, Post> postsMap = new HashMap<>();
